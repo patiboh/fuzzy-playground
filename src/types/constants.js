7342ac38cbf149927d9ecb097893ddfc8f61b56f
patiboh <lapatiboh@gmail.com>
2020-07-26 20:56:41 +0200

    🚧 Add types folder & constants

---
+++ src/types/constants.js
@@ -0,0 +1,28 @@
+export const uiStates = {
+  DEFAULT: 'default',
+  SUCCESS: 'success',
+  FOCUS: 'focus',
+  HOVER: 'hover',
+  ACTIVE: 'active',
+  ERROR: 'error',
+}
+export const size = {
+  LG: 'LG',
+  SM: 'SM',
+}
+export const emojis = {
+  body: {
+    default: 0x1f941, // 🥁 drums
+    success: 0x2728, // ✨ sparkles
+    error: 0x1f47b, // 👻 ghost
+  },
+  animate: {
+    default: 0x26a1, // ⚡️ lightning
+    success: 0x1f64c, // 🙌 raised handss
+    error: 0x1f52b, // 🔫 water pistol // OR 0x1f327 // 🌧 cloud with rain
+    active: 0x1f4a5, // 💥 fire spark
+  },
+  refresh: {
+    default: 0x1f9fd, // 🧽 sponge
+  },
+}
