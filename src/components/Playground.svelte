fa38fcc0beff7889e08a382bbbe9313f68bad38c
patiboh <lapatiboh@gmail.com>
2020-08-16 12:56:21 +0200

    🚧 Move everything to Playground to figure out event orchestration

--- src/components/Playground.svelte
+++ src/components/Playground.svelte
@@ -1,56 +1,48 @@
 <script>
+  // @ts-check
   import {onMount} from 'svelte'
 
+  import * as constants from '../types/constants.js'
   import * as utils from '../libs/utils.js'
   import * as draw from '../libs/draw.js'
 
-  import Display from '../components/Display.svelte'
-  import Controls from '../components/Controls.svelte'
+  let xPosition = 0
+  let yPosition = 0
+  let animateButton
+  let refreshButton
+  let feedback
+  let stacktrace
 
-  const state = {
-    DEFAULT: 'default',
-    SUCCESS: 'success',
-    FOCUS: 'focus',
-    HOVER: 'hover',
-    ACTIVE: 'active',
-    ERROR: 'error',
-  }
-  const size = {
-    LG: 'LG',
-    SM: 'SM',
-  }
-  const emojis = {
-    body: {
-      default: 0x1f941, // 🥁 drums
-      success: 0x2728, // ✨ sparkles
-      error: 0x1f47b, // 👻 ghost
-    },
-    animate: {
-      default: 0x26a1, // ⚡️ lightning
-      success: 0x1f64c, // 🙌 raised handss
-      error: 0x1f52b, // 🔫 water pistol // OR 0x1f327 // 🌧 cloud with rain
-      active: 0x1f4a5, // 💥 fire spark
-    },
-    refresh: {
-      default: 0x1f9fd, // 🧽 sponge
-    },
+  function surprise(characters) {
+    return new Array(100)
+      .fill(0)
+      .map((_, i) => {
+        return {
+          character: characters[i % characters.length],
+          x: Math.random() * 100,
+          y: -10 - Math.random() * 100,
+          radius: 0.1 + Math.random() * 1,
+        }
+      })
+      .sort((a, b) => a.radius - b.radius)
   }
 
-  function handleAnimate(event) {
-    const animateButton = event.target
-    // Output
-    const feedback = document.getElementById('feedback')
-    const stacktrace = document.getElementById('stack-trace')
-
-    animateButton.classList.add('active')
-    utils.updateCursor(animateButton, emojis.animate[state.ACTIVE], size.SM)
-    const confetti = document.getElementsByClassName('confetti')
-    const poop = document.getElementsByClassName('poop')
+  function handleAnimate() {
+    animateButton.classList.add(constants.uiState.ACTIVE)
+    utils.updateCursor(
+      animateButton,
+      constants.emojis.animate[constants.uiState.ACTIVE],
+      constants.size.SM,
+    )
+    const confetti = document.getElementsByClassName(
+      constants.confetti.CONFETTI,
+    )
+    const poop = document.getElementsByClassName(constants.confetti.POOP)
     Array.from(confetti).map((element) => {
-      element.classList.remove('yay')
+      element.classList.remove(constants.confetti.YAY)
     })
     Array.from(poop).map((element) => {
-      element.classList.remove('nay')
+      element.classList.remove(constants.confetti.NAY)
     })
     try {
       const webGlProps = draw.initScene()
@@ -59,44 +51,110 @@
       }, 1)
 
       window.setTimeout(() => {
-        animateButton.classList.remove('active')
-        animateButton.classList.remove('error')
-        animateButton.classList.add('success')
+        animateButton.classList.remove(constants.uiState.ACTIVE)
+        animateButton.classList.remove(constants.uiState.ERROR)
+        animateButton.classList.add(constants.uiState.SUCCESS)
         // this = animate button
-        utils.updateCursor(document.body, emojis.body[state.SUCCES], size.LG)
-        utils.updateCursor(animateButton, emojis.animate[state.SUCCES], size.SM)
+        utils.updateCursor(
+          document.body,
+          constants.emojis.body[constants.uiState.SUCCES],
+          constants.size.LG,
+        )
+        utils.updateCursor(
+          animateButton,
+          constants.emojis.animate[constants.uiState.SUCCES],
+          constants.size.SM,
+        )
         Array.from(confetti).map((element) => {
-          element.classList.add('yay')
+          element.classList.add(constants.confetti.YAY)
         })
         window.clearInterval(webGlAnimation)
       }, 1000)
     } catch (error) {
-      animateButton.classList.remove('active')
-      animateButton.classList.remove('success')
-      animateButton.classList.add('error')
-      document.body.classList.add('error')
-      feedback.classList.add('error')
+      animateButton.classList.remove(constants.uiState.ACTIVE)
+      animateButton.classList.remove(constants.uiState.SUCCESS)
+      animateButton.classList.add(constants.uiState.ERROR)
+      document.body.classList.add(constants.uiState.ERROR)
+      feedback.classList.add(constants.uiState.ERROR)
       stacktrace.append(`${error}\n`)
-      utils.updateCursor(document.body, emojis.body[state.ERROR], size.LG)
-      utils.updateCursor(animateButton, emojis.animate[state.ERROR], size.SM)
+      utils.updateCursor(
+        document.body,
+        constants.emojis.body[constants.uiState.ERROR],
+        constants.size.LG,
+      )
+      utils.updateCursor(
+        animateButton,
+        constants.emojis.animate[constants.uiState.ERROR],
+        constants.size.SM,
+      )
       Array.from(poop).map((element) => {
-        element.classList.add('nay')
+        element.classList.add(constants.confetti.NAY)
       })
       console.error(error)
     }
   }
 
   function handleRefresh(event) {
-    console.log('REFRESH')
-    utils.updateCursor(document.body, emojis.body[state.DEFAULT], size.LG)
-    utils.updateCursor(animateButton, emojis.animate[state.DEFAULT], size.SM)
+    utils.updateCursor(
+      document.body,
+      constants.emojis.body[constants.uiState.DEFAULT],
+      constants.size.LG,
+    )
+    utils.updateCursor(
+      animateButton,
+      constants.emojis.animate[constants.uiState.DEFAULT],
+      constants.size.SM,
+    )
     location.reload()
   }
 
   onMount(() => {
-    utils.updateCursor(document.body, emojis.body[state.DEFAULT], size.LG)
+    utils.updateCursor(
+      document.body,
+      constants.emojis.body[constants.uiState.DEFAULT],
+      constants.size.LG,
+    )
+    utils.updateCursor(
+      animateButton,
+      constants.emojis.animate[constants.uiState.DEFAULT],
+      constants.size.SM,
+    )
+    utils.updateCursor(
+      refreshButton,
+      constants.emojis.refresh[constants.uiState.DEFAULT],
+      constants.size.SM,
+    )
   })
 </script>
 
-<Display />
-<Controls on:animate={handleAnimate} on:refresh={handleRefresh} />
+<section class="display">
+  <canvas id="canvas" />
+  <div bind:this={feedback} data-cy="feedback">
+    <pre bind:this={stacktrace} />
+  </div>
+</section>
+
+<aside class="controls">
+  <div class="coordinates">
+    <label>
+      x = {xPosition}
+      <input type="range" bind:value={xPosition} />
+    </label>
+    <label>
+      y = {yPosition}
+      <input type="range" bind:value={yPosition} />
+    </label>
+  </div>
+  <button
+    data-cy="button-animate"
+    on:click={handleAnimate}
+    bind:this={animateButton}
+    class="jumbo fire-starter"
+    aria-label="Animate" />
+  <button
+    data-cy="button-refresh"
+    on:click={handleRefresh}
+    bind:this={refreshButton}
+    class="jumbo shower"
+    aria-label="Refresh" />
+</aside>
