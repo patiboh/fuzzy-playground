ca76a08cf51554374aa60461e41552eefffae262
patiboh <lapatiboh@gmail.com>
2020-12-27 18:54:13 +0100

    ✨ Translation via shader (fix shader :P)

--- src/gl/vertex-shader-translate-gl-2d.js
+++ src/gl/vertex-shader-translate-gl-2d.js
@@ -11,7 +11,7 @@ void main() {
   vec2 position = a_position + u_translation;
 
   // convert the position from pixels to 0.0 to 1.0
-  vec2 zeroToOne = a_position / u_resolution;
+  vec2 zeroToOne = position / u_resolution;
 
   // convert from 0->1 to 0->2
   vec2 zeroToTwo = zeroToOne * 2.0;
