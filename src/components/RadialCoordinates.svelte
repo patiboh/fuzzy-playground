3f9af9580015598a004a16b1ac6f71f05c362ff0
patiboh <lapatiboh@gmail.com>
2020-12-27 20:38:07 +0100

    🚧 Add RadialCoordinates component

---
+++ src/components/RadialCoordinates.svelte
@@ -0,0 +1,50 @@
+<script>
+  import {createEventDispatcher} from 'svelte'
+
+  const dispatch = createEventDispatcher()
+
+  // @ts-check
+  export let xRad = 0
+  export let yRad = 0
+  export let maxX
+  export let maxY
+
+  const handleXRadChange = () => {
+    dispatch('updateXRad', {
+      value: xRad,
+    })
+  }
+  const handleYRadChange = () => {
+    dispatch('updateYRad', {
+      value: yRad,
+    })
+  }
+</script>
+
+<style lang="scss">
+  @import '../styles/radial-coordinates.scss';
+</style>
+
+<div class="radial-coordinates" data-cy="radial-coordinates">
+  <!-- <label>
+     // TODO
+    x =
+    {xRad}
+    <input
+      data-cy="coord-x"
+      type="range"
+      bind:value={xRad}
+      max={maxX}
+      on:input={handleXRadChange} />
+  </label>
+  <label>
+    y =
+    {yRad}
+    <input
+      data-cy="coord-y"
+      type="range"
+      bind:value={yRad}
+      max={maxY}
+      on:input={handleYRadChange} />
+  </label> -->
+</div>
